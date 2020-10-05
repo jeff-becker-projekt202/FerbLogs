e10aca9936b1c4d2e78528e744481b0d84947705
Jeffrey Becker <jeffrey.becker@projekt202.com>
2020-10-05 08:26:44 -0500

    switch to anonymous class

--- src/Cfg/HandlerRef.php
+++ src/Cfg/HandlerRef.php
@@ -33,10 +33,45 @@ class HandlerRef
         if (!isset($all_handlers[$this->ref_name])) {
             return null;
         }
-        $result = new FilteringHandler($this->level, $this->prefix, $this->filter, $all_handlers[$this->ref_name]);
-        if (isset($this->formatter)) {
-            $result->setFormatter($this->formatter);
-        }
+        $result = new class ($this->level, $this->prefix, $this->filter, $all_handlers[$this->ref_name], $this->formatter) extends \Monolog\Handler\HandlerWrapper
+        {
+            private $filter;
+            private $level;
+            private $channel;
+        
+            public function __construct($level, $channel, $filter, $inner, $formatter)
+            {
+                parent::__construct($inner);
+                $this->filter = $filter;
+                $this->level = $level;
+                $this->channel = $channel;
+                if(!empty($formatter)){
+                    $this->setFormatter($formatter);
+                }
+            }
+        
+            public function isHandling(array $record)
+            {
+                if (1 == count(array_keys($record)) && isset($record['level'])) {
+                    return $record['level'] >= $this->level;
+                }
+                $is_this_channel = '*' == $this->channel || (0 == strpos($record['channel'], $this->channel));
+                $is_higher_level = $record['level'] >= $this->level;
+                $f = $this->filter;
+                $is_allowed = $f($record);
+        
+                return $is_allowed && $is_this_channel && $is_higher_level;
+            }
+        
+            public function handle(array $record)
+            {
+                if ($this->isHandling($record)) {
+                    return $this->handler->handle($record);
+                }
+        
+                return true;
+            }
+        };
 
         return $result;
     }
